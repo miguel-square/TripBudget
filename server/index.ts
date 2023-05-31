c0d6ba2eb9ceb882d41c620675629563baf2c9a5
Miguel Carrillo <miguel.carrillo@outsquare.com>
2023-05-31 11:45:14 -0500

    feat(swagger): Create Users Swagger Definition

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
-import express, { Request, Response } from "express";
+import express, { json, urlencoded, Request, Response } from "express";
+import swaggerUI from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 import {
   userRouter,
   eventUserRouter,
@@ -7,15 +9,42 @@ import {
   userExpenseRouter,
 } from "./routers/index";
 
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "TripBud App",
+    version: "1.0.0",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000",
+      description: "Local development server",
+    },
+  ],
+};
+
+const openapiSpecification = swaggerJSDoc({
+  swaggerDefinition,
+  apis: ["./routers/*.ts"],
+});
+
 const app = express();
 const port = 3000;
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 app.use("/user", userRouter);
 app.use("/event", eventRouter);
 app.use("/expense", expenseRouter);
 app.use("/user_expense", userExpenseRouter);
 app.use("/event_user", eventUserRouter);
-
 app.get("/", (req: Request, res: Response) => res.send("Hello World"));
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(openapiSpecification));
+
+app.use("/swagger.json", (req: Request, res: Response) =>
+  res.json(openapiSpecification).status(200)
+);
 
 app.listen(port, () => console.log(`Application started on port: ${port}`));
